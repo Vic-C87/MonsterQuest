22027285b7a27589663c2e84e6f2b10bf443f331
Vic-C87 <102956878+Vic-C87@users.noreply.github.com>
2022-12-27 17:50:41 +0100

    Separation of concerns
    Mission 4

---
+++ MonsterQuest/Assets/Scripts/Managers/CombatManager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MonsterQuest
+{
+    public class CombatManager : MonoBehaviour
+    {
+        int myEnemyHP;
+        [SerializeField]
+        string myHeroDamageDice = "2d6";
+        int myHeroDamage;
+        [SerializeField]
+        string myHeroConstitutionDice = "d20";
+        [SerializeField]
+        int myHeroConstitution = 3;
+        int myConstitutionRoll;
+        bool myHeroIsSaved;
+
+        bool myEnemyIsAlive = true;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public void Simulate(List<string> someHeroes, Enemy anEnemy, ref bool aHeroesAlive)
+        {
+            if (aHeroesAlive)
+            {
+                myEnemyHP = anEnemy.myHP;
+                myEnemyIsAlive = true;
+
+                Console.WriteLine("The heroes " + StringHelper.JoinWithAnd(someHeroes) + " descend into the dungeon.");
+                Console.WriteLine(anEnemy.myName + " with " + myEnemyHP + " HP appears!");
+                while (myEnemyHP > 0 && someHeroes.Count > 0)
+                {
+                    for (int i = 0; i < someHeroes.Count; i++)
+                    {
+                        if (myEnemyHP == 0)
+                        {
+                            break;
+                        }
+                        myHeroDamage = DiceHelper.Roll(myHeroDamageDice);
+                        myEnemyHP -= myHeroDamage;
+                        if (myEnemyHP <= 0)
+                        {
+                            myEnemyHP = 0;
+                            myEnemyIsAlive = false;
+                        }
+                        Console.WriteLine(someHeroes[i] + " hits the " + anEnemy.myName + " for " + myHeroDamage + " damage. " + anEnemy.myName + " has " + myEnemyHP + " HP left.");
+                    }
+                    if (myEnemyIsAlive)
+                    {
+                        myConstitutionRoll = DiceHelper.Roll(myHeroConstitutionDice) + myHeroConstitution;
+                        myHeroIsSaved = myConstitutionRoll >= anEnemy.myConstitutionSaveNeeded;
+                        int attackedHeroIndex = DiceHelper.GetRandom(someHeroes.Count) - 1;
+                        Console.WriteLine("The " + anEnemy.myName + " attacks " + someHeroes[attackedHeroIndex] + "!");
+                        Console.Write(someHeroes[attackedHeroIndex] + " rolls a " + myConstitutionRoll);
+
+                        if (myHeroIsSaved)
+                        {
+                            Console.WriteLine(" and is saved from the attack.");
+                        }
+                        else
+                        {
+                            Console.WriteLine(" and fails to be saved...");
+                            someHeroes.RemoveAt(attackedHeroIndex);
+                        }
+                    }
+                }
+                if (!myEnemyIsAlive)
+                {
+                    Console.WriteLine("The " + anEnemy.myName + " collapses and the heroes celebrate their victory!");
+                }
+                else
+                {
+                    Console.WriteLine("The party has failed and the " + anEnemy.myName + " continues to attack unsuspecting adventurers.");
+                    aHeroesAlive = false;
+                }
+            }
+        }
+    }
+}
--- MonsterQuest/Assets/Scripts/Managers/GameManager.cs
+++ MonsterQuest/Assets/Scripts/Managers/GameManager.cs
@@ -8,31 +8,30 @@ namespace MonsterQuest
 {
     public class GameManager : MonoBehaviour
     {
+        [SerializeField]
         List<string> myHeroes = new List<string>();
-        int myEnemyHP;
-        const string myHeroDamageDice = "2d6";
-        int myHeroDamage;
-        const string myHeroConstitutionDice = "d20";
-        const int myHeroConstitution = 3;
-        int myConstitutionRoll;
-        bool myHeroIsSaved;
-
-        bool myEnemyIsAlive = true;
+        
         bool myHeroesAreAlive = true;
 
         Enemy myOrc;
         Enemy myAzer;
         Enemy myTroll;
 
-        void Start()
+        CombatManager myCombatManager;
+
+        void Awake()
         {
-            SetHeroList("Jazlyn", "Theron", "Dayana", "Rolando");
+            myCombatManager = GetComponentInChildren<CombatManager>();
             myOrc = new Enemy("Orc", "2d8+6", 10);
             myAzer = new Enemy("Azer", "6d8+12", 18);
             myTroll = new Enemy("Troll", "8d10+40", 16);
-            SimulateBattle(myOrc);
-            SimulateBattle(myAzer);
-            SimulateBattle(myTroll);
+        }
+
+        void Start()
+        {
+            myCombatManager.Simulate(myHeroes, myOrc, ref myHeroesAreAlive);
+            myCombatManager.Simulate(myHeroes, myAzer, ref myHeroesAreAlive);
+            myCombatManager.Simulate(myHeroes, myTroll, ref myHeroesAreAlive);
             if (myHeroesAreAlive)
             {
                 string stillAlive;
@@ -46,87 +45,20 @@ namespace MonsterQuest
                 }
                 Console.WriteLine("After three grueling battles, the hero" + stillAlive + " return from the dungeons to live another day.");
             }
-        }
+        }      
+    }
 
-        void SimulateBattle(Enemy anEnemy)
+    public struct Enemy
+    {
+        public string myName;
+        public int myHP;
+        public int myConstitutionSaveNeeded;
+
+        public Enemy(string aName, string aDiceNotation, int aConstitutionSaveNeeded)
         {
-            if (myHeroesAreAlive)
-            {
-                myEnemyHP = anEnemy.myHP;
-                myEnemyIsAlive = true;
-
-                Console.WriteLine("The heroes " + StringHelper.JoinWithAnd(myHeroes) + " descend into the dungeon.");
-                Console.WriteLine(anEnemy.myName + " with " + myEnemyHP + " HP appears!");
-                while (myEnemyHP > 0 && myHeroes.Count > 0)
-                {
-                    for (int i = 0; i < myHeroes.Count; i++)
-                    {
-                        if (myEnemyHP == 0)
-                        {
-                            break;
-                        }
-                        myHeroDamage = DiceHelper.Roll(myHeroDamageDice);
-                        myEnemyHP -= myHeroDamage;
-                        if (myEnemyHP <= 0)
-                        {
-                            myEnemyHP = 0;
-                            myEnemyIsAlive = false;
-                        }
-                        Console.WriteLine(myHeroes[i] + " hits the " + anEnemy.myName + " for " + myHeroDamage + " damage. " + anEnemy.myName + " has " + myEnemyHP + " HP left.");
-                    }
-                    if (myEnemyIsAlive)
-                    {
-                        myConstitutionRoll = DiceHelper.Roll(myHeroConstitutionDice) + myHeroConstitution;
-                        myHeroIsSaved = myConstitutionRoll >= anEnemy.myConstitutionSaveNeeded;
-                        int attackedHeroIndex = DiceHelper.GetRandom(myHeroes.Count) - 1;
-                        Console.WriteLine("The " + anEnemy.myName + " attacks " + myHeroes[attackedHeroIndex] + "!");
-                        Console.Write(myHeroes[attackedHeroIndex] + " rolls a " + myConstitutionRoll);
-
-                        if (myHeroIsSaved)
-                        {
-                            Console.WriteLine(" and is saved from the attack.");
-                        }
-                        else
-                        {
-                            Console.WriteLine(" and fails to be saved...");
-                            myHeroes.RemoveAt(attackedHeroIndex);
-                        }
-                    }
-                }
-                if (!myEnemyIsAlive)
-                {
-                    Console.WriteLine("The " + anEnemy.myName + " collapses and the heroes celebrate their victory!");
-                }
-                else
-                {
-                    Console.WriteLine("The party has failed and the " + anEnemy.myName + " continues to attack unsuspecting adventurers.");
-                    myHeroesAreAlive = false;
-                }
-            }
-        }
-
-        void SetHeroList(string aFirstName, string aSeconName, string aThridName, string aFourthName)
-        {
-            myHeroes.Add(aFirstName);
-            myHeroes.Add(aSeconName);
-            myHeroes.Add(aThridName);
-            myHeroes.Add(aFourthName);
-        }
-
-        
-
-        struct Enemy
-        {
-            public string myName;
-            public int myHP;
-            public int myConstitutionSaveNeeded;
-
-            public Enemy(string aName, string aDiceNotation, int aConstitutionSaveNeeded)
-            {
-                myName = aName;
-                myHP = DiceHelper.Roll(aDiceNotation);
-                myConstitutionSaveNeeded = aConstitutionSaveNeeded;
-            }
+            myName = aName;
+            myHP = DiceHelper.Roll(aDiceNotation);
+            myConstitutionSaveNeeded = aConstitutionSaveNeeded;
         }
     }
 }
